b446ecb9cf858ee52b062bbcef6d4b3951f0b2a4
Hugo Fusinato <hugohvf@hotmail.com>
2020-04-17 21:55:43 -0300

    Envionment variables set

---
+++ src/environment/environment.js
@@ -0,0 +1,41 @@
+var _Environments = {
+
+  production:  {
+    SERVER_URL: '',
+    GEOCODE_KEY: '',
+  },
+  
+  staging:     {
+    BASE_URL: '', API_KEY: ''
+  },
+
+  development: {
+    BASE_URL: '', API_KEY: ''
+  },
+
+}
+
+function getPlatform(n) {
+  switch(n) {
+    case 0:
+      return 'production'
+      break;
+    case 1:
+      return 'staging'
+      break;
+    case 2:
+      return 'production'
+      break;
+  }
+}
+
+function getEnvironment() {
+  // Insert logic here to get the current platform (e.g. staging, production, etc)
+  var platform = getPlatform(0)
+
+  // ...now return the correct environment
+  return _Environments[platform]
+}
+
+var Environment = getEnvironment()
+module.exports = Environment
